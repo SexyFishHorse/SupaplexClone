b3fa1a49fab595219f8b2d25e30d78b20044c6f4
Asbjørn Hansen <ah@fenris.io>
2017-05-03 22:37:56 +0200

    add murphy and basic movement logic

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var direction = Vector3.zero;
+        var numKeyDown = 0;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            numKeyDown++;
+            direction = Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            numKeyDown++;
+            direction = Vector3.right;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            numKeyDown++;
+            direction = Vector3.up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            numKeyDown++;
+            direction = Vector3.down;
+        }
+
+        if (direction != Vector3.zero && numKeyDown == 1)
+        {
+            transform.position += direction * Time.deltaTime * 10;
+        }
+    }
+}
